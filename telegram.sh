bb8eb5a3d05dfbb88eaef4b85730d1442a22d2a2
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-07-13 15:00:41 +0700

    change telegram script

--- telegram.sh
+++ telegram.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-TOKEN=''
+token=''
 
-which curl &>/dev/null
-if [ $? -ne 0 ] ; then echo 'FAIL: curl not found.' && exit 1 ; fi
-if [ $# -ne 3 ] ; then echo 'FAIL: Params not defined.' && echo 'Usage: zabbix-to-telegram-notify.sh TelegramID Subject Message' && exit 1 ; fi
+chat="$1"
+subj="$2"
+message="$3"
 
-CHAT_ID="$1"
-SUBJECT="$2"
-MESSAGE="$3"
-
-curl -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"${SUBJECT}\n${MESSAGE}\"}" "https://
-api.telegram.org/bot${TOKEN}/sendMessage" | grep -q '"ok":false,'
-if [ $? -eq 0 ] ; then exit 1 ; fi
+/usr/bin/curl -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${chat}\",\"text\":\"${subj}\n${message}\"}" "https://api.telegram.org/bot${token}/sendMessage"
